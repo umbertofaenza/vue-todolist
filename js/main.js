9a4b07d760f514d72a2491a438220c7f9f187b79
umbertofaenza <umberto.faenza@gmail.com>
2023-08-31 15:23:42 +0200

    feat: Milestone 1

--- js/main.js
+++ js/main.js
@@ -3,7 +3,21 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      message: "Hello Vue",
+      pageTitle: "To Do!",
+      todoList: [
+        {
+          text: "Fare la spesa",
+          done: false,
+        },
+        {
+          text: "Prendere appuntamento dal dentista",
+          done: true,
+        },
+        {
+          text: "Chiamare agenzia immobiliare",
+          done: false,
+        },
+      ],
     };
   },
 }).mount("#app");
